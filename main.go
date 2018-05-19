37a2e8fece48ed1b79faa8f345ecb4ecb6f97625
1000ch <shogo.sensui@gmail.com>
2018-05-19 15:29:39 +0900

    List files

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-  "fmt"
+	"flag"
+	"fmt"
+	"io/ioutil"
 )
 
+func walkDir(arg string) []string {
+	var fileList []string
+
+	pathList, _ := ioutil.ReadDir(arg)
+	for _, path := range pathList {
+		name := path.Name()
+		if path.IsDir() {
+			fileList = append(fileList, walkDir(name)...)
+		} else {
+			fileList = append(fileList, name)
+		}
+	}
+
+	return fileList
+}
+
 func main() {
-  fmt.Println("jpg2png")
+	flag.Parse()
+
+	var fileList []string
+	for _, arg := range flag.Args() {
+		fileList = append(fileList, walkDir(arg)...)
+	}
+
+	for _, file := range fileList {
+		fmt.Println(file)
+	}
 }
