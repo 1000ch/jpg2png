448a4b40ee906095c33880a98e3968ae962087d8
1000ch <shogo.sensui@gmail.com>
2018-05-19 15:37:52 +0900

    Distinct files

--- main.go
+++ main.go
@@ -23,6 +23,20 @@ func walkDir(arg string) []string {
 	return fileList
 }
 
+func distinct(args []string) []string {
+	var fileList []string
+	appeared := map[string]bool{}
+
+	for _, arg := range args {
+		if !appeared[arg] {
+			appeared[arg] = true
+			fileList = append(fileList, arg)
+		}
+	}
+
+	return fileList
+}
+
 func main() {
 	flag.Parse()
 
@@ -31,7 +45,7 @@ func main() {
 		fileList = append(fileList, walkDir(arg)...)
 	}
 
-	for _, file := range fileList {
+	for _, file := range distinct(fileList) {
 		if strings.HasSuffix(file, ".jpg") {
 			fmt.Println(file)
 		}
